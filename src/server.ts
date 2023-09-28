7bc0514dff2b010c15dd32e44f0a4a81cea57589
Gabriel LK <gabrielfelipe941@yahoo.com.br>
2023-09-28 11:51:37 -0300

    headers cors

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,33 @@ import { AppError } from "./utils/AppError";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "https://lk-cards.vercel.app",
+      "http://lk-cards.vercel.app",
+      "http://lkcards-api.onrender.com",
+      "https://lkcards-api.onrender.com",
+    ],
+    credentials: true,
+  })
+);
+
+// try cors header
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method == "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+
+  next();
+});
 
 app.use("/files", express.static(UPLOAD_FOLDER));
 
@@ -55,20 +81,6 @@ io.on("connection", (socket) => {
     console.log("User disconnected", activeUsers);
     socket.emit("get-users", activeUsers);
   });
-});
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method == "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-
-  next();
 });
 
 const PORT = 8080;
